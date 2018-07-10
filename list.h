b98117e5da8703f7834ae84074b63334f5a74d7d
Igor Tyukalov <tyukalov@bk.ru>
2018-07-10 21:57:29 +0300

    Init

---
+++ list.h
@@ -0,0 +1,121 @@
+/* list.h
+ *  
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/* Igor 'sp_r00t' Tyukalov <tyukalov@bk.ru> */
+
+#include <stdlib.h>
+
+enum typelem{
+  NONE,
+  ATOM,
+  LIST
+};
+
+enum busy {
+  BUSY,
+  NOTBUSY
+};
+
+typedef struct {
+  void *data;
+  enum typelem type;
+} atomic;
+
+struct list {
+  atomic atom;
+  struct list   *prev;
+  struct list   *next;
+  enum busy busyed;
+};
+
+void print(struct list*);
+
+static inline struct list*
+create(){
+  struct list *res = (struct list*)calloc(1, sizeof(struct list));
+  if (res){
+    res->atom.type = NONE;
+    res->busyed = NOTBUSY;
+  }
+  return res;
+}
+
+static inline struct list*
+cdr(struct list* lst){
+  return lst->next;
+}
+
+static inline atomic
+car(struct list* lst){
+  return lst->atom;
+}
+
+static inline void
+lfree(struct list *lst){
+  if(lst){
+    lst->busyed = BUSY;
+    if(lst->next)
+      if(lst->next->busyed == NOTBUSY)
+	lfree(lst->next);
+    if(lst->prev)
+      if(lst->prev->busyed == NOTBUSY)
+	lfree(lst->prev);
+    if(lst->atom.data){
+      if(lst->atom.type == ATOM)
+	free(lst->atom.data);
+      else if (lst->atom.type == LIST)
+	lfree(lst->atom.data);
+    }
+    free(lst);
+  }
+}
+
+
+#define get(atom) ((atom).data)
+
+#define init_empty_element(e, t, lt, pl) {                              \
+  if (((pl)->atom.type = lt) == LIST) {					\
+    (pl)->atom.data = (void*)(e);					       	\
+  } else {								\
+    (pl)->atom.data = malloc(sizeof(t));			                \
+    if((pl)->atom.data)							\
+      *((t*)((pl)->atom.data)) = (e);				       	\
+  }									\
+}
+
+#define push(elem, tp, ltype, plst) ({				\
+    /* ltype - typelem */						\
+    if ((plst)->atom.type == NONE) {						\
+      init_empty_element(elem, tp, ltype, plst);			\
+    } else {								\
+      (plst)->prev = create();						\
+      if ((plst)->prev) {						\
+	init_empty_element(elem, tp, ltype, (plst)->prev);		\
+	((plst)->prev)->next = (plst);					\
+      }									\
+    }									\
+    ((plst)->prev)?                                                     \
+      ((((plst)->prev)->atom.data)?((plst)->prev):NULL): \
+      (((plst)->atom.data)?(plst):NULL);		 \
+    })
+
+#define pop(pplst) ({						       \
+  /* pplst - pplist */                                                         \
+  struct list *var = *(pplst);                                                 \
+  (var)?                                                                       \
+    ({*(pplst) = (*(pplst))->next;					\
+      var->atom.data;}):			\
+  NULL;                                                                        \
+    })
---
+++ test/list.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../list.h"
+  
+int
+main() {
+  struct list *a = create();
+  struct list *b = create();
+  struct list *c;
+  /* pplist pb = &b; */
+  struct list *r1;
+  int r2;
+  b = push(11, int, ATOM, b);
+  if(!b){
+    printf("Error\n");
+    return -1;
+  }
+  b = push(13, int, ATOM, b);
+  if(!b){
+    printf("Error\n");
+    return -1;
+  }
+  a = push(17, int, ATOM, a);
+  if(!a){
+    printf("Error\n");
+    return -1;
+  }
+  b = push(a, struct list*, LIST, b);
+  if(!b){
+    printf("Error\n");
+    return -1;
+  }
+  c=b;
+  print(b);
+  fputs("========================\n", stdout);
+  printf("car(b = )");
+  print((struct list*)get(car(b)));
+  printf("car(cdr(b)) = %d\n", *((int*)get(car(cdr(b)))));
+  r1 = pop(&b);
+  fputs("Print b\n", stdout);
+  print(b);
+  fputs("Print r1\n", stdout);
+  print(r1);
+  r2 = *((int*)(pop(&b)));
+  fputs("Print b\n", stdout);
+  print(b);
+  lfree(b);
+  /* lfree(a); */
+  return 0;
+}
+
+void print(struct list *lst){
+  if (!lst){
+    fputs("Error - zero pointer!\n", stdout);
+    return;
+  }
+  do{
+    if (lst->atom.type == ATOM){
+      printf("%d\n", (int)*((int *)(lst->atom.data)));
+    }
+    else if (lst->atom.type == LIST){
+      print(lst->atom.data);
+    }
+    lst = lst->next;
+  } while(lst);
+}
+
